99c7b96216480f6727e29cf2bd012b40bcf9d02f
Vishal Sharma <vsh1819@gmail.com>
2018-08-13 03:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
 var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 
 
