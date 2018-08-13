30eefb68d94408e2d05c18818ca5fa348d7dd778
Vishal Sharma <vsh1819@gmail.com>
2018-08-13 03:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
+
+
+
+
+  
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  $.get("", function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#results").html(response);
+  });
+
+
+
 });
